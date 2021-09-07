99350ab002edb999e38eef3d56357518d9e83d00
Jeason <jitong.zjt@alibaba-inc.com>
2021-09-07 16:50:40 +0800

    chore: add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,41 @@
-import { defineConfig } from 'vite';
-import * as path from 'path';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from "vite";
+import camelCase from "camelcase";
+import * as path from "path";
+import * as fs from "fs";
+import dts from "vite-plugin-dts";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import pkg from "./package.json";
+
+const getEntry = () => {
+  const tsxFile = path.resolve(__dirname, "src/index.tsx");
+  if (fs.existsSync(tsxFile)) return tsxFile;
+  const tsFile = path.resolve(__dirname, "src/index.ts");
+  if (fs.existsSync(tsFile)) return tsFile;
+  throw new Error("Cannot find entry. (src/index.tsx or src/index.ts)");
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    dts({ tsConfigFilePath: "./tsconfig.build.json", insertTypesEntry: true }),
+  ],
   esbuild: {
-    jsxInject: `import React from 'react';`,
+    jsxInject: `import * as React from 'react';`,
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'MyLib',
+      entry: getEntry(),
+      name: camelCase(path.basename(pkg.name)),
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'styled-components'],
+      external: ["react", "react-dom", "styled-components"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'styled-components': 'styled',
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
         },
       },
     },
